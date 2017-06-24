d8e6f018c3091dff230959155824889edc728dc2
kksrsoraumi <ingrid891110@gmail.com>
2017-06-24 12:49:44 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>FGISC x CKEISC 22th</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/bootstrap/css/build.css" rel="stylesheet">
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+    <!-- Theme CSS -->
+    <link href="css/grayscale.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    <i class="fa fa-play-circle"></i> <span class="light">FGISC x CKEISC</span> 22nd
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#polaris">FGISC - Polaris</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#crux">CKEISC - Crux</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#signUp">Sign Up</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <p class="brand-heading">FGISC<br>x<br>CKEISC</p>
+                        <p class="intro-text">Polaris & Crux
+                            <br>21st x 22nd 交接茶會</p>
+                        <a href="#about" class="btn btn-circle page-scroll">
+                            <i class="fa fa-angle-double-down animated"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- About Section -->
+    <section id="about" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <p class="titles">About </p>
+                <p class="pra">閃耀的星空將帶給各位一場精采絕倫的茶會<br>
+                   讓Polaris && Crux點燃你們的熱情<br>
+                   我們是穿梭在銀河中的建北電資<br>
+                   白洞、白色的明天正等著你們
+                   所以請準備墨鏡<br>
+                                        by北七<br>
+                    (以上言論不代表本台立場)
+               </p>
+                
+            </div>
+        </div>
+    </section>
+
+    <!-- Polaris Section -->
+    <section id="polaris" class="content-section text-center">
+        <div class="star-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2" align="center">
+                    <p class="titles">FGISC - Polaris</p>
+                    <p class="pra">2017.07.05 <br>
+                       FGISC 30th x 31st</p>
+                    <table class="tables">
+                        <tr id="table-title"><td>Time</td> <td>Activity</td> <td>      </td> <td>接待地點：</td></tr>
+                        <tr><td>12:40 - 13:20</td> <td>集合</td> <td>      </td> <td>捷運小南門站4號出口</td></tr>
+                        <tr><td>13:20 - 13:30</td> <td>幹部介紹</td> <td>      </td> <td>北一女中校門口</td></tr>
+                        <tr><td>13:30 - 14:00</td> <td>友社介紹</td> </tr>
+                        <tr><td>14:00 - 14:25</td> <td>公關時間</td> <td>      </td> <td id="table-title">活動地點：</td></tr>
+                        <tr><td>14:25 - 16:05</td> <td>大地遊戲</td> <td>      </td> <td>北一女中</td></tr>
+                        <tr><td>16:05 - 16:15</td> <td>休息時間</td></tr>
+                        <tr><td>16:15 - 16:55</td> <td>學姊時間</td> <td>      </td> <td>注意：因為北一女規定</td></tr>
+                        <tr><td>16:55 - 17:05</td> <td>幹部交接</td> <td>      </td> <td>外校生進入本校需著制服。</td></tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Crux Section -->
+    <section id="crux" class="content-section text-center">
+        <div class="star-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2" align="center">
+                    <p class="titles">CKEISC - Crux</p>
+                    <p class="pra">2017.07.06 <br>
+                       CKEISC 36th x 37th</p>
+                    <table class="tables" >
+                        <tr id="table-title"><td>Time</td> <td>Activity</td>  <td>      </td> <td>接待地點：</td></tr>
+                        <tr><td>13:30 - 14:00</td> <td>友社簽到</td> <td>      </td> <td>捷運中正紀念堂站2號出口</td></tr>
+                        <tr><td>14:00 - 14:10</td> <td>幹部介紹</td> <td>      </td> <td>建國中學校門口</td></tr>
+                        <tr><td>14:10 - 14:25</td> <td>友社介紹</td></tr>
+                        <tr><td>14:25 - 14:40</td> <td>公關時間</td> <td>      </td> <td id="table-title">活動地點：</td></tr>
+                        <tr><td>14:40 - 16:40</td> <td>大地遊戲</td> <td>      </td> <td>建國中學</td></tr>
+                        <tr><td>16:40 - 17:00</td> <td>學長時間</td></tr>
+                        <tr><td>17:00 - 17:30</td> <td>幹部交接</td></tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact" class="container content-section text-center">
+        <div class="container">
+            <div class="col-lg-8 col-lg-offset-2" align="center">
+                <p class="titles">Contact</p>
+                <table class="tables">
+                        <tr id="table-title"><td>北資執秘</td> <td>      </td> <td>建電執秘</td></tr>
+                        <tr><td>賴辰瑄（模糊）0911007235</td> <td>      </td> <td>梅洛凡（梅子）0935661908</td></tr>
+                        <tr id="table-title"><td>小南門接待</td> <td>      </td> <td>中正紀念堂接待</td></tr>
+                        <tr><td>劉亭妘（阿空）0978760850</td> <td>      </td> <td>徐捷（夏特羅）0922280924</td></tr>
+                        <tr id="table-title"><td>北一校門接待</td> <td>      </td> <td>建中校門接待</td></tr>
+                        <tr><td>譚書旻（北七）0903372313</td> <td>      </td> <td>王顥雲（ㄌㄑ）0989645009</td></tr>
+                    </table>
+            </div>
+        </div>
+    </section>
+
+    <!-- Sign Up Section -->
+    <?php
+    $Name = $Nickname = $school = $gender = $id =  $place_fg = $place_ck = $stay = $allSession = "";
+    $session = array();
+    $idErr = 0 ;
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (!empty($_POST["Name"])) {
+            $Name = test_input($_POST["Name"]);
+        } 
+        if (!empty($_POST["Nickname"])) {
+            $Nickname = test_input($_POST["Nickname"]);
+        } 
+        if (!empty($_POST["school"])) {
+            $school = test_input($_POST["school"]);
+        } 
+        if (!empty($_POST["id"])) {
+            $id = test_input($_POST["id"]);
+            if (!checkTwID($id)) {
+                $idErr = 1;  
+            }
+            else {
+                $idErr = 0;
+            }
+        }
+        if (!empty($_POST["gender"])) {
+            $gender = $_POST["gender"];
+        } 
+        if (!empty($_POST["session"])) {
+            $session = $_POST["session"];
+            $allSession = implode (" ", $session);
+        } 
+        if (!empty($_POST["place_fg"])) {
+            $place_fg = $_POST["place_fg"];
+        } 
+        if (!empty($_POST["place_ck"])) {
+            $place_ck = $_POST["place_ck"];
+        } 
+        if (!empty($_POST["stay"])) {
+            $stay = $_POST["stay"];
+        } 
+
+        if (!empty($_POST["Name"])&&!empty($_POST["Nickname"])&&!empty($_POST["school"])&&!empty($_POST["id"])&&!empty($_POST["gender"])&&!empty($allSession)&&!empty($_POST["place_fg"])&&!empty($_POST["place_ck"])&&!empty($_POST["stay"])) {
+            if ($idErr == 1){
+                phpalert("請填寫正確的身分證字號");
+            }
+            else{
+                $con = mysqli_connect("localhost","id1967751_ckefgisc22th","ckefgisc22","id1967751_ckefgisc");
+                $con -> query("SET NAMES 'utf8'");
+                $Main = $con -> prepare("INSERT INTO `joiner`(`name`, `nickname`, `school`, `id`, `gender`, `session`, `place_fg`, `place_ck`, `stay`) VALUES (?,?,?,?,?,?,?,?,?)");
+                $Main -> bind_param('sssssssss',$Name,$Nickname,$school,$id,$gender,$allSession,$place_fg,$place_ck,$stay);
+                $Main -> execute();
+                $Main -> close();
+                phpalert("報名成功!");
+                $con -> close();
+                refresh();
+            }
+        }
+        else {
+            phpalert("有欄位尚未填寫喔！");
+        }
+        
+    }
+
+    function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+    }
+    function checkTwID($id){
+        $city = array('A'=>1,'I'=>39,'O'=>48,'B'=>10,'C'=>19,'D'=>28,
+            'E'=>37,'F'=>46,'G'=>55,'H'=>64,'J'=>73,'K'=>82,
+            'L'=>2,'M'=>11,'N'=>20,'P'=>29,'Q'=>38,'R'=>47,
+            'S'=>56,'T'=>65,'U'=>74,'V'=>83,'W'=>21,'X'=>3,
+            'Y'=>12,'Z'=>30);
+        if (!preg_match("/[A-Z][1-2]\d{8}/",$id = strtoupper($id)) || strlen($id) != 10){
+            return false;
+        }
+         else {
+            //計算總分
+            $total = $city[$id[0]];
+            for($i=1;$i<=8;$i++){
+                $total += $id[$i] * (9 - $i);
+            }
+            //補上檢查碼(最後一碼)
+            $total += $id[9];
+            //檢查比對碼(餘數應為0);
+            return (($total%10 === 0 ));
+        }
+    }
+    function phpAlert($msg) {
+        echo '<script type="text/javascript">alert("' . $msg . '")</script>';
+    }
+    function refresh(){
+        echo '<script type="text/javascript">window.location = "https://ckefgisc22th.000webhostapp.com";</script>';
+    }
+    ?>
+    <section id="signUp" class="content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2" align="center">
+                <p class="titles">Sign Up</p>
+                <form class="SignUpForm" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                    姓名：<input class="in_text" type="text" name="Name" value="<?php echo $Name;?>"> <br>
+                    綽號：<input class="in_text" type="text" name="Nickname" value="<?php echo $Nickname;?>"> <br>
+                    學校社團：<input class="in_text" id="school" type="text" name="school" value="<?php echo $school;?>"> <br>
+                    身分證字號：<input class="in_text" id="ID" type="text" name="id" value="<?php echo $id;?>"> <br>
+                    生理性別：<input class="radio-info" type="radio" name="gender" value="M"<?php if (isset($gender) && $gender=="M") echo "checked";?>> 男 <input type="radio" name="gender" value="F" <?php if (isset($gender) && $gender=="F") echo "checked";?>> 女 <br>
+                    參加場次：<input type="checkbox" name="session[]" value="fg"<?php if (isset($session) && in_array("fg",$session)) echo "checked";?>>7/5 北一資研 Polaris <input type="checkbox" name="session[]" value="ck"<?php if (isset($session) && in_array("ck",$session)) echo "checked";?>>7/6 建中電研 Crux<br>
+                    集合地點（7/5）：<input type="radio" name="place_fg" value="MRT_fg" <?php if (isset($place_fg) && $place_fg=="MRT_fg") echo "checked";?>>捷運台大醫院站<input type="radio" name="place_fg" value="gate_fg" <?php if (isset($place_fg) && $place_fg=="gate_fg") echo "checked";?>>北一校門<input type="radio" name="place_fg" value="no_fg" <?php if (isset($place_fg) && $place_fg=="no_fg") echo "checked";?>>不參加<br>
+                    集合地點（7/6）：<input type="radio" name="place_ck" value="MRT_ck" <?php if (isset($place_ck) && $place_ck=="MRT_ck") echo "checked";?>>捷運中正紀念堂站<input type="radio" name="place_ck" value="gate_ck" <?php if (isset($place_ck) && $place_ck=="gate_ck") echo "checked";?>>建中校門<input type="radio" name="place_ck" value="no_ck" <?php if (isset($place_ck) && $place_ck=="no_ck") echo "checked";?>>不參加<br>
+                    是否住宿？<input type="radio" name="stay" value="Y" <?php if (isset($stay) && $stay=="Y") echo "checked";?>>是 <input type="radio" name="stay" value="N" <?php if (isset($stay) && $stay=="N") echo "checked";?>>否 <p>住宿費750元（有可能變動）</p>
+                    <input type="submit" value="送出" class="btn-default">                
+                </form>
+            </div>
+        </div>
+    </section>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        
+    }
+    ?>
+
+
+
+    <!-- Map Section 
+    <div id="map"></div>-->
+
+    <!-- Footer -->
+    <footer>
+        <div class="container text-center">
+            <p>Copyright &copy; 建北電資22nd 新鮮的肝們</p>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/grayscale.min.js"></script>
+
+</body>
+
+</html>
